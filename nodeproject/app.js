9a1e3d3a1bdc960796bf3cf7e7a808da6e3ef06c
Luis Gabriel N Simas <autorgabrielsimas@gmail.com>
2017-06-04 06:08:56 +0000

    Adicao do ExpressJs

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,16 @@
-var gabriel = 'Alo Mundo';
-console.log(gabriel);
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT;
+
+app.get('/',function(req,res){
+    res.send('Ola Mundo de novo!');
+});
+
+app.get('/routing',function(req,res){
+    res.send('Em Roteamento!');
+});
+
+app.listen(port,function(err){
+   console.log("O Servidor esta sendo executado na porta...: " + port); 
+});
