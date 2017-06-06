8d650d39338efaf016820e570b116dede1f0fcef
Luis Gabriel N Simas <autorgabrielsimas@gmail.com>
2017-06-06 01:42:57 +0000

    Bower.js e alteracoes no projeto principal

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -3,6 +3,17 @@ var app = express();
 
 var port = process.env.PORT;
 
+//Roda o que está no diretório "public" do projeto
+app.use(express.static('public'));
+
+//Perite rodar o diretório de views para entrar a index
+app.use(express.static('src/views'));
+
+//Deixa o diretório dos componentes do Bower visíveis para acesso
+//E mude os caminhos nos arquivos que utilizam os caminhos antigos dos pacotes
+//que agora são gerenciados pelo Bower
+app.use(express.static('bower_components'));
+
 app.get('/',function(req,res){
     res.send('Ola Mundo de novo!');
 });
